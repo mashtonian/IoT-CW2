f4d8be836f7017d8c6755c0d87649ae1b51604b6
mashtonian <66258035+mashtonian@users.noreply.github.com>
2022-04-09 13:12:26 +0100

    Update new_http_server.py
    Added tube status upload.
    Allowed for a header line in the data spreadsheet.

--- new_http_server.py
+++ new_http_server.py
@@ -1,3 +1,4 @@
+import requests
 from _thread import *
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
@@ -12,6 +13,9 @@ GDOCS_OAUTH_JSON = "iot-cw2-346112-27af4799a4dc.json"
 GDOCS_SPREADSHEET_NAME = "IoT-CW2-Dashboard"
 # change the worksheet name accordingly
 GDOCS_WORKSHEET_NAME = "Data"
+
+
+API_URL = "https://api.tfl.gov.uk/line/mode/tube,overground/status"
 
 
 def login_open_sheet(oauth_key_file, spreadsheet_name, worksheet_name):
@@ -29,6 +33,102 @@ def login_open_sheet(oauth_key_file, spreadsheet_name, worksheet_name):
         sys.exit(1)
 
 
+class RepeatTimer(Timer):
+    def run(self):
+        while not self.finished.wait(self.interval):
+            self.function(*self.args, **self.kwargs)
+
+
+def threaded_client(connection):
+    global gate_zero_message_count, gate_one_message_count, gate_two_message_count
+
+    while True:
+        data = connection.recv(2048)
+        message = data.decode('utf-8').split('_')
+        print(message[0])
+        if message[0] == "Gate 0":
+            gate_zero_message_count += 1
+        elif message[0] ==  "Gate 1":
+            gate_one_message_count += 1
+        elif message[0] == "Gate 2":
+            gate_two_message_count += 1
+
+
+def upload_to_spreadsheet(data):
+    sheet = login_open_sheet(GDOCS_OAUTH_JSON, GDOCS_SPREADSHEET_NAME, GDOCS_WORKSHEET_NAME)
+    sheet.insert_row(data, index=2)
+
+
+def upload_data():
+    global gate_zero_message_count, gate_one_message_count, gate_two_message_count
+    
+    data = [gate_zero_message_count, gate_one_message_count, gate_two_message_count]
+    tube_data.update()
+
+    lines = ["Piccadilly", "Victoria", "London Overground"]
+
+    for line in lines:
+       data.append(tube_data.data[line]["State"])
+    print('Uploading :')
+    print(data)
+    upload_to_spreadsheet(data)
+    gate_zero_message_count = 0
+    gate_one_message_count = 0
+    gate_two_message_count = 0
+
+
+def parse_api_response(response):
+    """Parse the TFL API json response."""
+    lines = [line["name"] for line in response]
+    data_dict = dict.fromkeys(lines)
+
+    for line in response:
+        try:
+            statuses = [
+                status["statusSeverityDescription"] for status in line["lineStatuses"]
+            ]
+            state = " + ".join(sorted(set(statuses)))
+
+            if (
+                state == "Good Service"
+            ):  # if good status, this is the only status returned
+                reason = "Nothing to report"
+            else:
+                reason = " *** ".join(
+                    [status["reason"] for status in line["lineStatuses"]]
+                )
+
+            data_dict[line["name"]] = {"State": state, "Description": reason}
+
+        except:
+            data_dict[line["name"]] = {
+                "State": None,
+                "Description": "Error parsing API data",
+            }
+
+    return data_dict
+
+
+class TubeData:
+    """Get the latest tube data from TFL."""
+
+    def __init__(self):
+        """Initialize the TubeData object."""
+        self._data = {}
+
+    def update(self):
+        """Get the latest data from TFL."""
+        response = requests.get(API_URL)
+        if response.status_code != 200:
+            return
+        self._data = parse_api_response(response.json())
+
+    @property
+    def data(self):
+        """Return the data."""
+        return self._data
+  
+    
 ServerSocket = socket.socket()
 ServerSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 host = "0.0.0.0"
@@ -45,51 +145,15 @@ gate_zero_message_count = 0
 gate_one_message_count = 0
 gate_two_message_count = 0
 
+tube_data = TubeData()
 
-class RepeatTimer(Timer):
-    def run(self):
-        while not self.finished.wait(self.interval):
-            self.function(*self.args, **self.kwargs)
-
-
-def threaded_client(connection):
-    global gate_zero_message_count, gate_one_message_count, gate_two_message_count
-
-    while True:
-        data = connection.recv(2048)
-        message = data.decode('utf-8').split('_')
-        print(message[0])
-        match message[0]:
-            case "Gate 0":
-                gate_zero_message_count += 1
-            case "Gate 1":
-                gate_one_message_count += 1
-            case "Gate 2":
-                gate_two_message_count += 1
-
-
-def upload_to_spreadsheet(counts):
-    sheet = login_open_sheet(GDOCS_OAUTH_JSON, GDOCS_SPREADSHEET_NAME, GDOCS_WORKSHEET_NAME)
-    sheet.insert_row(counts)
-
-
-def upload_count():
-    global gate_zero_message_count, gate_one_message_count, gate_two_message_count
-    counts = [gate_zero_message_count, gate_one_message_count, gate_two_message_count]
-    print('Uploading :')
-    print(counts)
-    upload_to_spreadsheet(counts)
-    gate_zero_message_count = 0
-    gate_one_message_count = 0
-    gate_two_message_count = 0
-
-
-upload_timer = RepeatTimer(UPLOAD_INTERVAL, upload_count)
+upload_data()
+upload_timer = RepeatTimer(UPLOAD_INTERVAL, upload_data)
 upload_timer.start()
 
 while True:
     Client, address = ServerSocket.accept()
     print('Connected to: ' + address[0] + ':' + str(address[1]))
     start_new_thread(threaded_client, (Client, ))
+    
 ServerSocket.close()
-
